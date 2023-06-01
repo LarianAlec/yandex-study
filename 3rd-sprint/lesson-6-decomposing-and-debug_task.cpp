0c6d6f16e94265b337958f1446a6747b2a7b4250
Larin Alec <lay9997@yandex.ru>
2023-06-02 01:07:54 +0300

    2 from 10 tests complete

--- 3rd-sprint/lesson-6-decomposing-and-debug_task.cpp
+++ 3rd-sprint/lesson-6-decomposing-and-debug_task.cpp
@@ -23,7 +23,28 @@ struct Query {
 };
 
 istream& operator>>(istream& is, Query& q) {
-    // Реализуйте эту функцию
+    string command;
+    is >> command;
+
+    if (command == "NEW_BUS"s) {
+        q.type = QueryType::NewBus;
+        is >> q.bus;
+        int stops_count;
+        is >> stops_count;
+        q.stops.resize(stops_count);
+        for(int i = 0; i < stops_count; ++i) {
+            is >> q.stops[i];
+        }
+
+    } else if (command == "BUSES_FOR_STOP"s) {
+        q.type = QueryType::BusesForStop;
+        is >> q.stop;
+    } else if (command == "STOPS_FOR_BUS"s) {
+        q.type = QueryType::StopsForBus;
+        is >> q.bus;
+    } else if (command == "ALL_BUSES"s) {
+        q.type = QueryType::AllBuses;
+    }
     return is;
 }
 
@@ -33,26 +54,64 @@ struct BusesForStopResponse {
 };
 
 ostream& operator<<(ostream& os, const BusesForStopResponse& r) {
-    // Реализуйте эту функцию
-    return os;
+    if (r.stop_name.empty()) {
+        os << "No stop"s;
+        return os;
+    } else {
+        for (const auto& bus : r.buses) {
+            os << bus << " "s;
+        }
+        return os;
+    }
 }
 
 struct StopsForBusResponse {
     string bus_name;
     vector<string> stops;
+    map<string, vector<string>> buses_for_stop;
 };
 
 ostream& operator<<(ostream& os, const StopsForBusResponse& r) {
-    // Реализуйте эту функцию
+    if (r.bus_name.empty()) {
+        os << "No bus"s;
+    } else {
+        for (const string& stop : r.stops) {
+        os << "Stop "s << stop << ": "s;
+        if (r.buses_for_stop.at(stop).size() == 1) {
+            os << "no interchange\n"s;
+            
+        } else {
+            for (const string& bus : r.buses_for_stop.at(stop)) {
+                if (bus == r.bus_name) continue;
+                os << bus << " "s;
+            }
+            if (stop == r.stops.back()) continue;
+            os << "\n"s;
+        }
+        }
+
+    }
     return os;
 }
 
 struct AllBusesResponse {
-    map<string, vector<string>> buses_for_stops;
+    map<string, vector<string>> buses_and_stops;
 };
 
 ostream& operator<<(ostream& os, const AllBusesResponse& r) {
-    // Реализуйте эту функцию
+    if (r.buses_and_stops.empty()) {
+        os << "No buses"s;
+    } else {
+        for (const auto& [bus_name, stops] : r.buses_and_stops) {
+            os << "Bus "s << bus_name << ": "s;
+            for (const string& stop : stops) {
+                os << stop << " "s;
+                if (stop == stops.back()) {
+                    os << "\n"s;
+                }
+            }
+        }
+    }
     return os;
 }
 
@@ -61,15 +120,15 @@ public:
     void AddBus(const string& bus, const vector<string>& stops) {
         stops_for_bus_[bus] = stops;
         for (const string& stop : stops) {
-            buses_for_stops_[stop].push_back(bus);
+            buses_for_stop_[stop].push_back(bus);
         }
     }
 
     BusesForStopResponse GetBusesForStop(const string& stop) const {
         BusesForStopResponse response;
-        if(buses_for_stops_.count(stop)) {
+        if(buses_for_stop_.count(stop)) {
             response.stop_name = stop;
-            response.buses = buses_for_stops_.at(stop);
+            response.buses = buses_for_stop_.at(stop);
         }
         return response;
     }
@@ -80,22 +139,22 @@ public:
             response.bus_name = bus;
             response.stops = stops_for_bus_.at(bus);
         }
+        response.buses_for_stop = buses_for_stop_;
         return response;
     }
 
     AllBusesResponse GetAllBuses() const {
-        AllBusesResponse response;
-        response.buses_for_stops = buses_for_stops_;
-        return response;
+        return AllBusesResponse {stops_for_bus_};
     }
 
 private:
-    map<string, vector<string>> buses_for_stops_, // маршруты автобуса на остановке
+    map<string, vector<string>> buses_for_stop_, // автобусы на остановке
                                 stops_for_bus_;  //остановки автобуса
 };
 
 
 // Модульные тесты
+
 void TestingNewBusMethod() {
     BusManager bm;
     bm.AddBus("golden_ring"s, {"sergiev_posad"s, "rostov"s, "ivanovo"s, "vladimir"s});
@@ -113,6 +172,7 @@ void TestingGetBusesForStopMethod() {
     assert(bm.GetBusesForStop("rostov"s).buses.size() == 2);
     assert(bm.GetBusesForStop("vladimir"s).buses.size() == 1);
 }
+
 
 
 // Тестирование
